73c9d4bcfad0e3cd62d4105170de9376d6b918d3
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-02-23 14:14:38 -0300

    added head and favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,15 @@
+import Head from 'next/head'
 import '../src/presentation/styles/globals.scss'
 
 function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    return (
+        <>
+            <Head>
+                <title>Dev It - Seja um DEV mais produtivo</title>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
         return (
             <Html>
                 <Head>
+                    <link rel="shortcut icon" href="/favicon-dev-it.svg" />
                     <link rel="preconnect" href="https://fonts.gstatic.com" />
                     <link
                         href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;900&family=Rajdhani:wght@600&display=swap"
